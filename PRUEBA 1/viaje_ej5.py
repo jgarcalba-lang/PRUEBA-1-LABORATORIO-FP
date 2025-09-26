6678594115ac74de9264d8078f343624b6ac3b5f
Jesús García <jesgaralb@alum.us.es>
2025-09-26 16:59:48 +0200

    Arreglao

--- PRUEBA 1/viaje_ej5.py
+++ PRUEBA 1/viaje_ej5.py
@@ -4,10 +4,15 @@ tiempo_horas = distancia_km // velocidad_kmh
 tiempo_dias = tiempo_horas // 24
 print(f"Tardarías {tiempo_dias} días en llegar.")
 opcion = input("¿Quieres hacer otra simulación? (s/n). ").lower()
-if opcion != "s":
-    print(f1)
-elif opcion != "n":
-    print("Programa terminado")
+while opcion == "s":
+    distancia_km = int(input("Introduce la distancia en km:"))   # distancia Tierra - Luna
+    velocidad_kmh = int(input("Introduce la velocidad en km/h:"))
+    tiempo_horas = distancia_km // velocidad_kmh
+    tiempo_dias = tiempo_horas // 24
+    print(f"Tardarías {tiempo_dias} días en llegar.")
+    opcion = input("¿Quieres hacer otra simulación? (s/n). ").lower()
+
+print("Programa terminado")
 
 
 
